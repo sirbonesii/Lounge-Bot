c8b05b2063669533c95c2ea4fca689493e04f3a4
sirbonesii <146753209+sirbonesii@users.noreply.github.com>
2023-10-03 15:45:16 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     discord: {
         id: process.env.DISCORD_ID,
         prefix: '!',
-        footer: `© Corwin 2021 - ${new Date().getFullYear()}`, 
-        botInvite: `https://discord.com/oauth2/authorize?&client_id=840212110817755157&scope=applications.commands+bot&permissions=8`,
-        serverInvite: "https://discord.gg/qHXxpQKC2H",
+        footer: `© Bonesii. 2023 - ${new Date().getFullYear()}`, 
+        botInvite: `https://discord.com/api/oauth2/authorize?client_id=1158477576709349456&permissions=8&scope=bot`,
+        serverInvite: "https://discord.gg/WJnccBufub",
     },
 
     wordList: `Airplane
